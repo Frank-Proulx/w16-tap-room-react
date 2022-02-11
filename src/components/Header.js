e0c171859b06ad3e124990bc57c2c7c3119b77d5
Frank-Proulx <frankproulx7@gmail.com>
2022-02-11 13:40:52 -0800

    WIP: a lot of css tinkering

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
+import beertaps from "./../img/beertaps.jpg";
 
 function Header(){
+  const headerImgStyles = {
+    width: '30vw',
+    height: 'auto',
+    display: 'block',
+    margin: '0 auto'
+  }
   return(
     <React.Fragment>
-      <h1>Tap Room Inventory</h1>
+      <div>
+        <img style={headerImgStyles} src={beertaps} alt="a group of beer taps"/>
+        <h1>Tap Room Inventory</h1>
+      </div>
     </React.Fragment>
   );
 }
